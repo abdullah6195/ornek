fa223b174b3c9d0eef33384e75f12a6f02e5ad1a
abdullah6195 <59163864+abdullah6195@users.noreply.github.com>
2021-12-18 14:05:09 +0300

    Create sayfalama.php

---
+++ sayfalama.php
@@ -0,0 +1,316 @@
+<?php
+
+$data = [
+    [
+        'title' => 'Başlık 1',
+        'img' => '//via.placeholder.com/286x180?text=Resim1',
+        'content' => 'Aliquam nec quam sem. Donec hendrerit bibendum mauris, porta iaculis.'
+    ],
+    [
+        'title' => 'Başlık 2',
+        'img' => '//via.placeholder.com/286x180?text=Resim2',
+        'content' => 'Etiam vitae massa sit amet metus maximus efficitur. Aliquam varius libero mi.'
+    ], [
+        'title' => 'Başlık 3',
+        'img' => '//via.placeholder.com/286x180?text=Resim3',
+        'content' => 'Cras felis neque, tempor vitae accumsan at, tempus imperdiet libero.'
+    ], [
+        'title' => 'Başlık 4',
+        'img' => '//via.placeholder.com/286x180?text=Resim4',
+        'content' => 'Donec gravida nisl sit amet convallis feugiat.'
+    ], [
+        'title' => 'Başlık 5',
+        'img' => '//via.placeholder.com/286x180?text=Resim5',
+        'content' => 'Vestibulum urna diam, eleifend quis felis id, lobortis tincidunt risus.'
+    ], [
+        'title' => 'Başlık 6',
+        'img' => '//via.placeholder.com/286x180?text=Resim6',
+        'content' => 'Vestibulum in vulputate leo, malesuada tincidunt nisl. Maecenas quis.'
+    ], [
+        'title' => 'Başlık 7',
+        'img' => '//via.placeholder.com/286x180?text=Resim7',
+        'content' => 'Sed nec feugiat ante, id consectetur sapien. Cras blandit sodales ante.'
+    ], [
+        'title' => 'Başlık 8',
+        'img' => '//via.placeholder.com/286x180?text=Resim8',
+        'content' => 'Mauris ut tempus dolor. Quisque posuere efficitur diam in pellentesque.'
+    ], [
+        'title' => 'Başlık 9',
+        'img' => '//via.placeholder.com/286x180?text=Resim9',
+        'content' => 'Donec vehicula odio nec velit eleifend elementum. Interdum et malesuada fames ac ante ipsum.'
+    ], [
+        'title' => 'Başlık 10',
+        'img' => '//via.placeholder.com/286x180?text=Resim10',
+        'content' => 'Donec iaculis odio nec tincidunt volutpat. Morbi at cursus orci, id molestie magna.'
+    ], [
+        'title' => 'Başlık 11',
+        'img' => '//via.placeholder.com/286x180?text=Resim11',
+        'content' => 'Nunc eget neque augue. Suspendisse dictum, turpis ut pellentesque ornare.'
+    ], [
+        'title' => 'Başlık 12',
+        'img' => '//via.placeholder.com/286x180?text=Resim12',
+        'content' => 'Donec et velit nulla. Maecenas vulputate luctus leo sed porta.'
+    ], [
+        'title' => 'Başlık 13',
+        'img' => '//via.placeholder.com/286x180?text=Resim13',
+        'content' => 'Nunc imperdiet euismod sapien a dignissim. Lorem ipsum dolor sit amet.'
+    ], [
+        'title' => 'Başlık 14',
+        'img' => '//via.placeholder.com/286x180?text=Resim14',
+        'content' => 'Nunc vitae hendrerit leo, non laoreet ex. Etiam laoreet ultrices lacus, ut consequat.'
+    ], [
+        'title' => 'Başlık 5',
+        'img' => '//via.placeholder.com/286x180?text=Resim5',
+        'content' => 'Vivamus molestie eleifend ligula vehicula vestibulum.'
+    ], [
+        'title' => 'Başlık 16',
+        'img' => '//via.placeholder.com/286x180?text=Resim16',
+        'content' => 'Vivamus eleifend enim eu rhoncus rhoncus. In eget congue nibh. Aenean quis cursus felis.'
+    ], [
+        'title' => 'Başlık 17',
+        'img' => '//via.placeholder.com/286x180?text=Resim17',
+        'content' => 'Ut at fermentum tortor, vel scelerisque nisl. In hac habitasse platea dictumst.'
+    ], [
+        'title' => 'Başlık 18',
+        'img' => '//via.placeholder.com/286x180?text=Resim18',
+        'content' => 'Pellentesque feugiat efficitur elit, nec finibus sapien.'
+    ], [
+        'title' => 'Başlık 19',
+        'img' => '//via.placeholder.com/286x180?text=Resim19',
+        'content' => 'Sed tempor dictum nibh, id interdum tellus. Fusce aliquet, urna nec aliquet mattis.'
+    ], [
+        'title' => 'Başlık 20',
+        'img' => '//via.placeholder.com/286x180?text=Resim20',
+        'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non ornare velit.'
+    ], [
+        'title' => 'Başlık 21',
+        'img' => '//via.placeholder.com/286x180?text=Resim21',
+        'content' => 'Fusce vestibulum feugiat urna, sed volutpat metus facilisis sit.'
+    ], [
+        'title' => 'Başlık 22',
+        'img' => '//via.placeholder.com/286x180?text=Resim22',
+        'content' => 'Duis in posuere leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices.'
+    ], [
+        'title' => 'Başlık 23',
+        'img' => '//via.placeholder.com/286x180?text=Resim23',
+        'content' => 'Sed eu justo et elit sollicitudin fringilla. Suspendisse sed mollis eros.'
+    ], [
+        'title' => 'Başlık 24',
+        'img' => '//via.placeholder.com/286x180?text=Resim24',
+        'content' => 'Suspendisse nisi est, finibus et eros tempus, porta luctus est..'
+    ], [
+        'title' => 'Başlık 25',
+        'img' => '//via.placeholder.com/286x180?text=Resim25',
+        'content' => 'Ut ornare nibh in nisi hendrerit, eu pulvinar lorem sodales.'
+    ], [
+        'title' => 'Başlık 26',
+        'img' => '//via.placeholder.com/286x180?text=Resim26',
+        'content' => 'Phasellus vitae lacinia felis, et facilisis ligula. In ac interdum neque.'
+    ], [
+        'title' => 'Başlık 27',
+        'img' => '//via.placeholder.com/286x180?text=Resim27',
+        'content' => 'Vivamus quis leo eleifend, sagittis velit nec, dapibus nibh.'
+    ], [
+        'title' => 'Başlık 28',
+        'img' => '//via.placeholder.com/286x180?text=Resim28',
+        'content' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada.'
+    ], [
+        'title' => 'Başlık 29',
+        'img' => '//via.placeholder.com/286x180?text=Resim29',
+        'content' => 'Morbi at dolor ac tortor auctor rhoncus. Curabitur consectetur elit.'
+    ], [
+        'title' => 'Başlık 30',
+        'img' => '//via.placeholder.com/286x180?text=Resim30',
+        'content' => 'Donec eget mauris sagittis, ultrices sapien at, gravida mauris.'
+    ], [
+        'title' => 'Başlık 31',
+        'img' => '//via.placeholder.com/286x180?text=Resim31',
+        'content' => 'Duis nec massa tincidunt, ultrices orci in, aliquam nisi. Ut et.'
+    ], [
+        'title' => 'Başlık 32',
+        'img' => '//via.placeholder.com/286x180?text=Resim32',
+        'content' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada.'
+    ], [
+        'title' => 'Başlık 33',
+        'img' => '//via.placeholder.com/286x180?text=Resim33',
+        'content' => 'Etiam egestas rhoncus interdum. Etiam id bibendum odio, et commodo nisl.'
+    ], [
+        'title' => 'Başlık 34',
+        'img' => '//via.placeholder.com/286x180?text=Resim34',
+        'content' => 'Nunc efficitur libero non lorem molestie viverra.'
+    ], [
+        'title' => 'Başlık 35',
+        'img' => '//via.placeholder.com/286x180?text=Resim35',
+        'content' => 'Cras egestas sem non tortor hendrerit, ac rhoncus nisl suscipit.'
+    ], [
+        'title' => 'Başlık 36',
+        'img' => '//via.placeholder.com/286x180?text=Resim36',
+        'content' => 'Duis metus nunc, ullamcorper et vestibulum eu, ullamcorper eu felis.'
+    ], [
+        'title' => 'Başlık 37',
+        'img' => '//via.placeholder.com/286x180?text=Resim37',
+        'content' => 'Quisque eu consectetur eros. Nulla viverra condimentum ante eget malesuada.'
+    ],
+    [
+        'title' => 'Başlık 38',
+        'img' => '//via.placeholder.com/286x180?text=Resim38',
+        'content' => 'Nulla vehicula tristique elit sit amet rutrum. Pellentesque sodales elementum ipsum.'
+    ],[
+        'title' => 'Başlık 39',
+        'img' => '//via.placeholder.com/286x180?text=Resim39',
+        'content' => 'Mauris erat dolor, dictum nec quam et, dignissim aliquam elit. Integer sit amet.'
+    ],[
+        'title' => 'Başlık 40',
+        'img' => '//via.placeholder.com/286x180?text=Resim40',
+        'content' => 'Nunc placerat egestas condimentum. Aenean ante felis, viverra ut dictum in, congue id est.'
+    ],[
+        'title' => 'Başlık 41',
+        'img' => '//via.placeholder.com/286x180?text=Resim41',
+        'content' => 'Praesent et dolor vitae lectus porta porttitor tristique vel mauris.'
+    ],[
+        'title' => 'Başlık 42',
+        'img' => '//via.placeholder.com/286x180?text=Resim42',
+        'content' => 'Pellentesque eget purus commodo, sollicitudin arcu non, interdum elit.'
+    ],[
+        'title' => 'Başlık 43',
+        'img' => '//via.placeholder.com/286x180?text=Resim43',
+        'content' => 'Cras convallis vehicula sem, ut rhoncus dui feugiat euismod.'
+    ],[
+        'title' => 'Başlık 44',
+        'img' => '//via.placeholder.com/286x180?text=Resim44',
+        'content' => 'Nunc sit amet nisi nisi. Pellentesque sagittis feugiat quam vel faucibus.'
+    ],[
+        'title' => 'Başlık 45',
+        'img' => '//via.placeholder.com/286x180?text=Resim45',
+        'content' => 'Phasellus porta molestie leo sit amet varius. Mauris a sagittis ipsum, id commodo nisi.'
+    ],[
+        'title' => 'Başlık 46',
+        'img' => '//via.placeholder.com/286x180?text=Resim46',
+        'content' => 'Proin tincidunt non dolor ac consequat. Aenean efficitur pulvinar nisl ut iaculis.'
+    ],[
+        'title' => 'Başlık 47',
+        'img' => '//via.placeholder.com/286x180?text=Resim47',
+        'content' => 'Aliquam consequat facilisis leo quis dignissim. Fusce rhoncus finibus fermentum.'
+    ],[
+        'title' => 'Başlık 48',
+        'img' => '//via.placeholder.com/286x180?text=Resim48',
+        'content' => 'Sed non ligula et justo ultricies egestas eget ac enim. Pellentesque malesuada.'
+    ],[
+        'title' => 'Başlık 49',
+        'img' => '//via.placeholder.com/286x180?text=Resim49',
+        'content' => 'Etiam accumsan facilisis dolor. Etiam vitae sapien vitae ligula.'
+    ],[
+        'title' => 'Başlık 50',
+        'img' => '//via.placeholder.com/286x180?text=Resim50',
+        'content' => 'Nunc ut consectetur sapien. Curabitur tristique libero vitae placerat.'
+    ],[
+        'title' => 'Başlık 51',
+        'img' => '//via.placeholder.com/286x180?text=Resim51',
+        'content' => 'Nullam consectetur varius tortor, vehicula semper sem vehicula sed.'
+    ],[
+        'title' => 'Başlık 52',
+        'img' => '//via.placeholder.com/286x180?text=Resim52',
+        'content' => 'Sed et purus pretium, rhoncus sem sed, egestas neque. Fusce sodales sagittis ultricies.'
+    ],[
+        'title' => 'Başlık 53',
+        'img' => '//via.placeholder.com/286x180?text=Resim53',
+        'content' => 'Suspendisse ac lorem orci. Nullam ac mauris augue. Donec fringilla risus a rhoncus suscipit.'
+    ],[
+        'title' => 'Başlık 54',
+        'img' => '//via.placeholder.com/286x180?text=Resim54',
+        'content' => 'Sed ipsum eros, fermentum et justo id, pretium dapibus ligula. Phasellus eget.'
+    ],[
+        'title' => 'Başlık 55',
+        'img' => '//via.placeholder.com/286x180?text=Resim55',
+        'content' => 'Integer eu tempus magna, eu eleifend metus. Maecenas ac eros id elit blandit blandit.'
+    ],[
+        'title' => 'Başlık 56',
+        'img' => '//via.placeholder.com/286x180?text=Resim56',
+        'content' => 'Pellentesque eu mi non sem consectetur vestibulum. Duis elementum.'
+    ],[
+        'title' => 'Başlık 57',
+        'img' => '//via.placeholder.com/286x180?text=Resim57',
+        'content' => 'In ex justo, molestie et fermentum ac, sodales sed diam.'
+    ],[
+        'title' => 'Başlık 58',
+        'img' => '//via.placeholder.com/286x180?text=Resim58',
+        'content' => 'Praesent lobortis est vitae justo tristique accumsan.'
+    ],[
+        'title' => 'Başlık 59',
+        'img' => '//via.placeholder.com/286x180?text=Resim59',
+        'content' => 'Sed sit amet ipsum ante. Nam tristique aliquet mauris, sed semper.'
+    ],[
+        'title' => 'Başlık 60',
+        'img' => '//via.placeholder.com/286x180?text=Resim60',
+        'content' => 'Curabitur scelerisque varius ornare. Quisque non aliquam nulla.'
+    ],[
+        'title' => 'Başlık 61',
+        'img' => '//via.placeholder.com/286x180?text=Resim61',
+        'content' => 'Quisque a lectus ac augue tempor iaculis. Proin bibendum consequat.'
+    ]
+];
+
+$page = $_GET['page'] ?? 1;
+$sayfada = 10;
+
+$count = count($data);
+$toplamSayfa = ceil($count / $sayfada);
+$page = $page > $toplamSayfa ? $toplamSayfa : $page;
+$sayfala = array_slice($data, (($page - 1) * $sayfada), $sayfada, true);
+?>
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row p-3">
+
+        <?php foreach ($sayfala as $a) { ?>
+            <div class="col col-sm-6 col-md-4 col-xl-2 mb-3">
+                <div class="card">
+                    <img class="card-img-top" src="<?= $a['img'] ?>" alt="Resim">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $a['title'] ?></h5>
+                        <p class="card-text"><?= $a['content'] ?></p>
+                        <a target="_blank" href="<?= str_replace('286x180', '500', $a['img']) ?>" class="btn btn-primary">Resme Git</a>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+
+        <?php if ($count > $sayfada) { ?>
+            <div class="col-12">
+                <nav>
+                    <ul class="pagination justify-content-end flex-wrap">
+                        <li class="page-item <?= $page == 1 ? 'disabled' : null ?>">
+                            <a class="page-link" href="?page=1" tabindex="-1">Birinci</a>
+                        </li>
+                        <li class="page-item <?= $page == 1 ? 'disabled' : null ?>">
+                            <a class="page-link" href="?page=<?= $page - 1 ?>" tabindex="-1">Önceki</a>
+                        </li>
+                        <?php for ($i = 1; $i <= $toplamSayfa; $i++) { ?>
+                            <li class="page-item <?= $page == $i ? 'active' : null ?>">
+                                <a class="page-link" href="<?= $page == $i ? 'javascript:void(0)' : '?page='.$i ?>"><?= $i ?></a>
+                            </li>
+                        <?php } ?>
+                        <li class="page-item <?= $page == $toplamSayfa ? 'disabled' : null ?>">
+                            <a class="page-link" href="?page=<?= $page + 1 ?>">Sonraki</a>
+                        </li>
+                        <li class="page-item <?= $page == $toplamSayfa ? 'disabled' : null ?>">
+                            <a class="page-link" href="?page=<?= $toplamSayfa ?>">Sonuncu</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        <?php } ?>
+    </div>
+</div>
+</body>
+</html>
